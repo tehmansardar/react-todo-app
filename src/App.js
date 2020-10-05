5178a3bf92949163dfe0503036a879554bcde329
Tehman Sardar <tehmansardar@gmail.com>
2020-10-05 18:05:13 +0500

    Create basic structure of Todo app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Breakfast First",
+    "Reading Novel",
+    "Check Email",
+  ]);
+  const [input, setInput] = useState("");
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Welcome to Todo List</h1>
+      <form onSubmit={addTodo}>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button type="submit">Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
